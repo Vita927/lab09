e19d5c52422dfc3c136f3938cee645d08329bea2
Vita927 <vita29.2007@gmail.com>
2026-05-17 16:51:41 +0300

    added cpack config

---
+++ _build/_CPack_Packages/Linux-Source/TGZ/print-...-Source/hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+//главная функций
+int main() {
+    std::string name;
+    std::cout << "Enter name: ";
+    std::cin >> name;
+    std::cout << "Hello world from " << name << std::endl;
+    return 0;
+}
