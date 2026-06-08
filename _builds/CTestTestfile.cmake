aaafda9fa579199d29c29433a0a762cf3c459ef3
Vita927 <vita29.2007@gmail.com>
2026-06-08 20:01:05 +0300

    add .gitignore to exclude build artifacts

--- _builds/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vita/Vita927/workspace/projects/lab07
-# Build directory: /home/vita/Vita927/workspace/projects/lab07/_builds
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(check "/home/vita/Vita927/workspace/projects/lab07/_builds/check")
-set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/vita/Vita927/workspace/projects/lab07/CMakeLists.txt;18;add_test;/home/vita/Vita927/workspace/projects/lab07/CMakeLists.txt;0;")
-subdirs("third-party/gtest")
--- _builds/third-party/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vita/Vita927/workspace/projects/lab07/third-party/gtest
-# Build directory: /home/vita/Vita927/workspace/projects/lab07/_builds/third-party/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
